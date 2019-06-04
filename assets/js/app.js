c862fd375f156695fb19d8ba636266972aa7f7a1
Nicole Arendt <nicole.arendt23@gmail.com>
2019-06-04 06:08:51 -0400

    Level 1 done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,105 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = {
+  top: 20,
+  right: 40,
+  bottom: 80,
+  left: 100
+};
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart,
+// and shift the latter by left and top margins.
+var svg = d3
+  .select("#scatter")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+// Append an SVG group
+var chartGroup = svg.append("g")
+  .attr("transform", `translate(${margin.left}, ${margin.top})`);
+
+// Retrieve data from the CSV file and execute everything below
+d3.csv("assets/data/data.csv").then(function(bigData, err) {
+  if (err) throw err;
+
+  console.log(bigData);
+
+  // parse data
+  bigData.forEach(function(data) {
+    data.poverty = +data.poverty;
+    data.healthcare = +data.healthcare;
+    data.age = +data.age; //need to change to median
+    data.smokes = +data.smokes;
+    data.income = +data.income; //need to change to median
+    data.obesity = +data.obesity;
+  });
+
+  // xLinearScale function above csv import
+  var xLinearScale = d3.scaleLinear()
+    .domain([d3.min(bigData, d=>d.poverty) -1, d3.max(bigData, d=>d.poverty) +1 ])
+    .range([0,width]);
+
+  // Create y scale function
+  var yLinearScale = d3.scaleLinear()
+    .domain([d3.min(bigData, d=>d.healthcare)-1, d3.max(bigData, d => d.healthcare) +1])
+    .range([height, 0]);
+
+  // Create initial axis functions
+  var bottomAxis = d3.axisBottom(xLinearScale);
+  var leftAxis = d3.axisLeft(yLinearScale);
+
+  // append x axis
+  chartGroup.append("g")
+    .classed("x-axis", true)
+    .attr("transform", `translate(0, ${height})`)
+    .call(bottomAxis);
+
+  // append y axis
+  chartGroup.append("g")
+    .call(leftAxis);
+
+  // append initial circles
+  var circlesGroup = chartGroup.selectAll("circle")
+    .data(bigData)
+    .enter()
+    .append("circle")
+    .attr("cx", d => xLinearScale(d.poverty))
+    .attr("cy", d => yLinearScale(d.healthcare))
+    .attr("r", 15)
+    .attr("fill", "lightblue")
+    .attr("opacity",".9");
+
+  chartGroup.selectAll()
+    .data(bigData)
+    .enter()
+    .append("text")
+    .attr("x", d => xLinearScale(d.poverty))
+    .attr("y", d => yLinearScale(d.healthcare))
+    .attr("text-anchor", "middle")
+    .attr("stroke", "white")
+    .attr("stroke-width", "2px")
+    .attr("dy",".3em")
+    .text(d => (d.abbr));
+
+  chartGroup.append("text")
+    .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+    .attr("value", "hair_length") // value to grab for event listener
+    .classed("active", true)
+    .text("In Poverty (%)");
+
+  // append y axis
+  chartGroup.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin.left)
+    .attr("x", 0 - (height / 2))
+    .attr("dy", "1em")
+    .attr("value", "healthcare")
+    .classed("axis-text", true)
+    .classed("active", true)
+    .text("Lacks Healthcare (%)");
+});
